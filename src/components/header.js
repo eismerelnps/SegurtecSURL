c9c6afb8617dced91740027986e0b34db4c92584
Eismer Lobaina <eismerlobaina@gmail.com>
2023-04-21 00:24:57 -0400

    First Commit

---
+++ src/components/header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export const header = () => {
+  return (
+    <div>
+    <img src="https://www.facebook.com/photo/?fbid=119303291058534&set=a.119303304391866&__tn__=%3C" alt="Company Logo" />
+     <nav> 
+        <ul>
+             <li>
+                <a href="">Home</a>
+                </li> <li><a href="">About Us</a>
+                </li> <li><a href="">Services</a>
+                </li> <li><a href="">Contact</a>
+                </li> 
+                </ul>
+                 </nav>
+</div>
+  )
+}
+
