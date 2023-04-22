e22b82e995f6cc34a576837e61f2daa920bce0cb
Eismer Lobaina <eismerlobaina@gmail.com>
2023-04-21 23:37:28 -0400

    Buid created

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,7 +4,6 @@ export const Footer = () => {
   return (
     <div className='footer'>
         <p > SegurTec SURL</p>
-        <img alt='facebook logo' src='https://th.bing.com/th/id/OIP.GZZGcE7ZibytK97LNApTBAHaHa?w=182&h=181&c=7&r=0&o=5&pid=1.7'/>
         </div>
   )
 }
