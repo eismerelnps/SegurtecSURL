800722085cc69aaddfbd785dbcaff3fa0558cf57
Eismer Lobaina <eismerlobaina@gmail.com>
2023-04-23 01:59:16 -0400

    Working on docs and local fotos

--- src/components/header.js
+++ src/components/header.js
@@ -2,12 +2,11 @@ import React from 'react'
 //import headerPhoto from ''
 
 export const Header = () => {
-  //const url1 ='../static/card-segurtec.png'
-  //const url2 = "https://scontent-bog1-1.xx.fbcdn.net/v/t39.30808-6/327954767_515179707393926_6599493625946629431_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=i3slHp18VawAX-Mma3u&_nc_ht=scontent-bog1-1.xx&oh=00_AfBA8dJlyVRPTavdC41tgD_FbmcflQhU9-xGq2wWQE9QKQ&oe=6448A565"
+  const url = require('docs/static/media/card-segurtec.0ce4706aa6b87b2ac671.png')
   return (
     <div className='header'>
    <img className='company-img'
-       src={require('../photos/card-segurtec.png')}
+       src={url}
         alt="Company Logo"
       />
 </div>
