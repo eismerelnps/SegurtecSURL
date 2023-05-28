4b31e2a5dab0f6544e941fd32c117e2fde1262a1
Eismer Lobaina <eismerlobaina@gmail.com>
2023-05-28 02:00:39 -0400

    Deploy new version

---
+++ docs/precache-manifest.06df3f7286f7bb2446c2dd49889ad5b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f84307677283db36c256",
+    "url": "/static/css/main.650b9c87.chunk.css"
+  },
+  {
+    "revision": "f84307677283db36c256",
+    "url": "/static/js/main.d0e4973c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1daca98be61613966f20",
+    "url": "/static/css/2.d59d0e7a.chunk.css"
+  },
+  {
+    "revision": "1daca98be61613966f20",
+    "url": "/static/js/2.b3c4f2be.chunk.js"
+  },
+  {
+    "revision": "ab5b575c169bfc336f3f8b2394e967e3",
+    "url": "/index.html"
+  }
+];
